e673b07bf0d73954b2b0c334add65bb4eb71ca9b
lhenkel <lhenkel@gmail.com>
2014-01-24 17:19:39 -0700

    added gggData.py (again)

--- gggData.py
+++ gggData.py
@@ -1,1 +1,13 @@
-/home/lhenkel/gggData.py
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import Series, DataFrame
+import KNGLib as k
+
+sf = k.SF('prd')
+ms = k.MSSQL('prd')
+
+print "Fired up k.SF as sf, so sf.do_query('SELECT Something')"
+print "Fired up k.MSSQL as ms, ms.do_query('SELECT SOMETHING')"
+print "Send alerts with k.alert('lee@kng.com','some subject','something happened') "
+print "This is PRD by the way"
